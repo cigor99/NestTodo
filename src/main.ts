5ef53b51fe5e8aacea931c4d063f769dcdc3ebb0
Igor Lukic <igi.l.1999@gmail.com>
2022-02-27 23:09:57 +0100

    Change origin

--- src/main.ts
+++ src/main.ts
@@ -4,7 +4,7 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    "origin": "d1bkuujn6bxyqb.cloudfront.net/",
+    "origin": "https://d1bkuujn6bxyqb.cloudfront.net",
     "methods": "GET,HEAD,PUT,PATCH,POST,DELETE",
     "preflightContinue": false,
     "optionsSuccessStatus": 204
